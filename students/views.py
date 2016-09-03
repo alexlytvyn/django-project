a54d72a338b05bd3d09ea5fc06ac3e5583d1b36a
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2016-09-03 21:35:31 +0300

    add templates for students list

--- students/views.py
+++ students/views.py
@@ -1,8 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 # Views for Students
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
+	students = (
+		{'id': 1,
+		'first_name': u'Олександр',
+		'last_name': u'Литвин',
+		'ticket': '007',
+		'image': 'img/alytvyn.jpg'},
+		{'id': 2,
+		'first_name': u'Вікторія',
+		'last_name': u'Мороз',
+		'ticket': '010',
+		'image': 'img/vmoroz.jpg'},
+		{'id': 3,
+		'first_name': u'Михайло',
+		'last_name': u'Желюк',
+		'ticket': 609,
+		'image': 'img/mzheljuk.jpg'},
+		)
+	return render(request, 'students/students_list.html',
+		{'students': students})
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
 def students_edit(request, sid):
