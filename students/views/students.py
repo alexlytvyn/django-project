c56ea7717451b3c251d0757bb989ffc08e9f85a6
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2016-09-05 17:25:09 +0300

    Views reorganization

--- students/views/students.py
+++ students/views/students.py
@@ -28,24 +28,3 @@ def students_edit(request, sid):
 	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 def students_delete(request, sid):
 	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
-# Views for Groups
-def groups_list(request):
-	groups = (
-		{'id': 1,
-		'name': 'Ф-07',
-		'leader': {'id': 1, 'name': u'Литвин Олександр'}},
-		{'id': 2,
-		'name': 'Ф-08',
-		'leader': {'id': 2, 'name': u'Мороз Вікторія'}},
-		{'id': 3,
-		'name': 'Ф-09',
-		'leader': {'id': 3, 'name': u'Желюк Михайло'}},
-		)
-	return render(request, 'students/groups_list.html',
-		{'groups': groups})
-def groups_add(request):
-	return HttpResponse('<h1>Group Add Form</h1>')
-def groups_edit(request, gid):
-	return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-def groups_delete(request, gid):
-	return HttpResponse('<h1>Delete Group %s</h1>' % gid)
