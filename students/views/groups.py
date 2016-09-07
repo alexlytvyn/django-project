026ad4d5200c0edeb6aa1c70f3663a404cbf8abf
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2016-09-07 13:24:31 +0300

    Add Group model, updated list of groups

--- students/views/groups.py
+++ students/views/groups.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from ..models import Group
 # Views for Groups
 def groups_list(request):
-	groups = (
-		{'id': 1,
-		'name': 'Ф-07',
-		'leader': {'id': 1, 'name': u'Литвин Олександр'}},
-		{'id': 2,
-		'name': 'Ф-08',
-		'leader': {'id': 2, 'name': u'Мороз Вікторія'}},
-		{'id': 3,
-		'name': 'Ф-09',
-		'leader': {'id': 3, 'name': u'Желюк Михайло'}},
-		)
+	groups = Group.objects.all()
+	# try to order groups list
+	order_by = request.GET.get('order_by', '')
+	if order_by in ('id', 'title'):
+		groups = groups.order_by(order_by)
+		if request.GET.get('reverse', '') == '1':
+			groups = groups.reverse()
+	# paginate groups
+	paginator = Paginator(groups, 3)
+	page = request.GET.get('page')
+	try:
+		groups = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		groups = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver
+		# last page of results.
+		groups = paginator.page(paginator.num_pages)
 	return render(request, 'students/groups_list.html',
 		{'groups': groups})
 def groups_add(request):
